d247177bcb713245c0f105c25220ed911991cf5f
wgd <wgd@qq.com>
2016-04-14 19:27:45 +0800

    decode.h

--- src/decode.h
+++ src/decode.h
@@ -5,8 +5,12 @@
 
 namespace td_proto {
 
-#define CHECK_BUFFER_VAILD(value) if (!buffer.isVaild()) { \
+#define CHECK_RETURN_BUFFER_VAILD(value) if (!buffer.isVaild()) { \
 	return value; \
+}
+
+#define CHECK_BREAK_BUFFER_VAILD() if (!buffer.isVaild()) { \
+	break; \
 }
 
 	Values decode_field(ByteBuffer& buffer, Config& config);
@@ -84,7 +88,7 @@ namespace td_proto {
 			if (field.is_nil_type()) {
 				return Values(map);
 			}
-			CHECK_BUFFER_VAILD(Values(map));
+			CHECK_RETURN_BUFFER_VAILD(Values(map));
 			auto sub_value = decode_field(buffer, config);
 			auto name = config.get_field_index_name(field.index);
 			if ( name.size() == 0 ) {
@@ -106,9 +110,10 @@ namespace td_proto {
 				buffer.setVaild(false);
 				break;
 			}
-			//value->push_back(sub_value);
+			CHECK_BREAK_BUFFER_VAILD();
+			value->push_back(std::move(sub_value));
 		}
-		return Values(value, pattern);
+		return Values(value, (u8)pattern);
 	}
 
 	Values decode_by_field(ByteBuffer& buffer, Config& config, Field& field) {
@@ -150,6 +155,32 @@ namespace td_proto {
 			return Values();
 		}
 		return decode_by_field(buffer, config, field);
+	}
+
+	std::tuple<std::string, std::vector<Values>> decode_proto(ByteBuffer& buffer, Config& config) {
+		auto name_value = decode_str_raw(buffer, TYPE_STR);
+		std::vector<Values> value;
+		while (true)
+		{
+			auto sub_value = decode_field(buffer, config);
+			if (sub_value.sub_type == TYPE_NIL) {
+				break;
+			}
+			CHECK_BREAK_BUFFER_VAILD();
+			value.push_back(std::move(sub_value));
+		}
+		auto result = std::make_pair(*name_value._str, std::move(value));
+		auto proto = config.get_proto_by_name(*name_value._str);
+		if (proto == NULL) {
+			buffer.setVaild(false);
+			return std::move(result);
+		}
+		if (proto->args.size() != value.size()) {
+			buffer.setVaild(false);
+			return std::move(result);
+		}
+		return std::move(result);
+
 	}
 
 }
