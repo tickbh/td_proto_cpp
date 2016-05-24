59db36cf705052da0cf462a0933844723217fd01
wgd <wgd@qq.com>
2016-05-24 10:26:14 +0800

    add const

--- include/Values.h
+++ include/Values.h
@@ -53,7 +53,7 @@ namespace td_proto {
 
 	struct cmp_str
 	{
-		bool operator()(char const *a, char const *b)
+		bool operator()(char const *a, char const *b) const
 		{
 			return strcmp(a, b) < 0;
 		}
